ba7b0ceb7db10c8d2765cc68a728827c2336f3f6
Joshua Osburn <joshuakosburn@gmail.com>
2021-10-28 23:59:13 -0400

    Updated controllers to work with MongoDB through Mongoose.

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,21 +1,70 @@
-const getAllTasks = (req, res) => {
-    res.send('getAllTasks');
+const Task = require('../models/Task');
+
+const getAllTasks = async (req, res) => {
+    try {
+        const tasks = await Task.find({});
+        res.status(200).json({ tasks });
+    } catch (err) {
+        res.status(500).json({ err });
+    }
 }
 
-const createTask = (req, res) => {
-    res.json(req.body);
+const createTask = async (req, res) => {
+    try {
+        const task = await Task.create(req.body);
+        res.status(201).json({ task });
+    } catch (err) {
+        res.status(500).json({ err });
+    }
 }
 
-const getTask = (req, res) => {
-    res.json(req.params.id);
+const getTask = async (req, res) => {
+    try {
+        const { id: taskID } = req.params;
+        const task = await Task.findOne({ _id: taskID });
+
+        if (!task) {
+            return res.status(404).json({ message: `Could not find task with ID: ${taskID}` });
+        }
+
+        res.status(200).json({ task });
+    } catch (err) {
+        res.status(500).json({ err });
+    }
 }
 
-const updateTask = (req, res) => {
-    res.send('updateTask');
+const updateTask = async (req, res) => {
+    try {
+        const { id: taskID } = req.params;
+        const task = await Task.findOneAndUpdate(
+            { _id: taskID },
+            req.body,
+            { new: true, runValidators: true }
+        );
+
+        if (!task) {
+            return res.status(404).json({ message: `Could not find task with ID: ${taskID}` });
+        }
+
+        res.status(200).json({ task });
+    } catch (err) {
+        res.status(500).json({ err });
+    }
 }
 
-const deleteTask = (req, res) => {
-    res.send('deleteTask');
+const deleteTask = async (req, res) => {
+    try {
+        const { id: taskID } = req.params;
+        const task = await Task.findOneAndDelete({ _id: taskID });
+
+        if (!task) {
+            return res.status(404).json({ message: `Could not find task with ID: ${taskID}` });
+        }
+
+        res.status(200).json({ message: `Task with ID ${taskID} has been deleted!`});
+    } catch (err) {
+        res.status(500).json({ err });
+    }
 }
 
 module.exports = {
